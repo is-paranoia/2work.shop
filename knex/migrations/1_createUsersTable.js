b851fd2dcf48fbc55812b9db5d477ecad5e95c0f
is-paranoia <55973519+is-paranoia@users.noreply.github.com>
2022-07-05 20:30:08 +0500

    add orders table migrations

--- knex/migrations/1_createUsersTable.js
+++ knex/migrations/1_createUsersTable.js
@@ -1,9 +1,14 @@
 exports.up = function (knex) {
     return knex.schema
         .createTable('Users', function(table) {
-            table.increments('id');
+            table.increments('id').primary();
+            table.string('nickname').notNullable();
             table.string('email').notNullable();
             table.string('password').notNullable();
+            table.string('wallet').notNullable();
+            //table.foreign('orders').references('id').inTable('Users');
+            table.boolean('isAdmin').notNullable().defaultTo(0);
+            table.datetime('lastLogin').notNullable().defaultTo(knex.fn.now());
             table.datetime('registerDate').notNullable().defaultTo(knex.fn.now());
         });
 };
--- routes/api.js
+++ routes/api.js
@@ -7,18 +7,34 @@ const router = Router()
 const knex = require('../knex/knex')
 
 
-router.get(
-    '/users',
-    (req, res) => {
-        try {
-            knex.raw('select * from "Users"').then((users) =>{
-                res.send(users.rows)
-            })
-        } catch (e) {
-            res.status(500).json({
-                message: "Server error"
-            })
-        }
-    })
+    router.get(
+        '/users',
+        (req, res) => {
+            try {
+                knex.raw('select * from "Users"').then((users) =>{
+                    res.send(users.rows)
+                })
+            } catch (e) {
+                res.status(500).json({
+                    message: "Server error"
+                })
+            }
+        })
+
+    router.get(
+        '/orders',
+        (req, res) => {
+            try {
+                knex.raw('select * from "Orders"').then((orders) =>{
+                    res.send(orders.rows)
+                })
+            } catch (e) {
+                res.status(500).json({
+                    message: "Server error"
+                })
+            }
+        })
+
+
 
 module.exports = router
