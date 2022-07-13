31d012044b3837b4541885bfafe476f5629a52e0
is-paranoia <55973519+is-paranoia@users.noreply.github.com>
2022-07-13 22:19:29 +0500

    Change migrations

--- knex/migrations/6_createCommentsTable.js
+++ knex/migrations/6_createCommentsTable.js
@@ -3,7 +3,7 @@ exports.up = function (knex) {
         .createTable('Comments', function(table) {
             table.increments('id').primary();
             table.integer('orderId').notNullable().references('id').inTable('Orders').index().onDelete("CASCADE");
-            table.integer('userId').notNullable().references('id').inTable('Users').index().onDelete("SET NULL");
+            table.integer('userId').notNullable().references('id').inTable('Users').index().onDelete("CASCADE");
             table.string('message').notNullable();
             table.datetime('createdAt').notNullable().defaultTo(knex.fn.now());
         });
