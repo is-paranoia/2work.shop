7fc9eda9320b5ed76284a82abd5890f36c890b40
is-paranoia <55973519+is-paranoia@users.noreply.github.com>
2022-07-18 00:21:19 +0500

    Style knex files and auth-middleware

--- knex/migrations/8_createRespondsTable.js
+++ knex/migrations/8_createRespondsTable.js
@@ -1,13 +1,13 @@
 exports.up = function (knex) {
-    return knex.schema
-        .createTable('Responds', function(table) {
-            table.increments('id').primary();
-            table.integer('orderId').notNullable().references('id').inTable('Orders').index().onDelete("CASCADE");
-            table.integer('userId').notNullable().references('id').inTable('Users').index().onDelete("CASCADE");
-        });
-};
+	return knex.schema
+		.createTable("Responds", function(table) {
+			table.increments("id").primary()
+			table.integer("orderId").notNullable().references("id").inTable("Orders").index().onDelete("CASCADE")
+			table.integer("userId").notNullable().references("id").inTable("Users").index().onDelete("CASCADE")
+		})
+}
 
 exports.down = function (knex) {
-    return knex.schema
-        .dropTable('Responds');
+	return knex.schema
+		.dropTable("Responds")
 }
--- knex/seeds/2_roles.js
+++ knex/seeds/2_roles.js
@@ -1,19 +1,17 @@
-exports.seed = function(knex, Promise) {
-    // Deletes ALL existing entries
-    return knex('Roles').del()
-    .then(function () {
-        // Inserts seed entries
-        return knex('Roles').insert([
-            {
-                id: 1,
-                permissionsId: 8,
-                name: 'defaultUser'
-            },
-            {
-                id: 2,
-                permissionsId: 10,
-                name: 'adminUser'
-            }
-      ]);
-    });
-  };
+exports.seed = function(knex) {
+	return knex("Roles").del()
+		.then(function () {
+			return knex("Roles").insert([
+				{
+					id: 1,
+					permissionsId: 8,
+					name: "defaultUser"
+				},
+				{
+					id: 2,
+					permissionsId: 10,
+					name: "adminUser"
+				}
+			])
+		})
+}
